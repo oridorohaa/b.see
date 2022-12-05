62c8b2d76b646a4fa68f52754f9c55e8845fa764
Andriana Oridoroha <oridorohaa@gmail.com>
2022-12-05 14:28:29 -0600

    main menu path created

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+  get "/menu", to: "pages#menu"
 end
