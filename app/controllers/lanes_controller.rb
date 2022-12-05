cdae9847b7bba9f3148c00399528e68034017397
Morgan Bell <mtbell98@gmail.com>
2022-12-05 14:01:05 -0600

    added lanes controller, routes, and views

---
+++ app/controllers/lanes_controller.rb
@@ -0,0 +1,53 @@
+class LanesController < ApplicationController
+  before_action :set_lane, only: %i[show edit update destroy]
+
+  def index
+    @lane = Lane.all
+  end
+
+  def show
+    # show
+  end
+
+  def new
+    @lane = Lane.new
+  end
+
+  def create
+    @lane = Lane.new(lane_params)
+    @lane.user = current_user
+    if @lane.save
+      redirect_to @lane
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    # edit
+  end
+
+  def update
+    @lane.update(lane_params)
+    if @lane.save
+      redirect_to @lane
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @lane.destroy
+    redirect_to lanes_path, status: :see_other
+  end
+
+  private
+
+  def set_lane
+    @lane = Lane.find(params[:id])
+  end
+
+  def lane_params
+    params.require(:lane).permit(:street, :start_point, :end_point, :description, :status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :bike_rakes
+  resources :lanes
 end
