f347835749b4974d11cf42239be3b7a1e73b6321
Boris Moreno <larefineriadedavin@gmail.com>
2022-12-05 16:53:32 -0600

    fixed to BikeRack

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,34 @@
 
 # html_file = URI.open(url).read
 # html_doc = Nokogiri::HTML(html_file)
-Racks.destroy_all
+BikeRack.destroy_all
 
-Racks.create!(address: "Adolfo Prieto 303, código 2, Col del Valle Centro, Benito Juárez, 03103 Ciudad de México, CDMX",
-              description: "Tres biciestacionamientos tipo cuadro")
+BikeRack.create!(address: "Adolfo Prieto 303, código 2, Col del Valle Centro, Benito Juárez, 03103 Ciudad de México, CDMX",
+                 description: "Tres biciestacionamientos tipo cuadro")
 
-Racks.create!(address: "Amsterdam 244, Hipódromo, Cuauhtémoc, 06100 Ciudad de México, CDMX",
-              description: "Un biciestacionamiento tipo U invertidad")
+BikeRack.create!(address: "Amsterdam 244, Hipódromo, Cuauhtémoc, 06100 Ciudad de México, CDMX",
+                 description: "Un biciestacionamiento tipo U invertidad")
 
-Racks.create!(address: "C. Amberes 6, Juárez, Cuauhtémoc, 06600 Ciudad de México, CDMX",
-              description: "Cinco biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "C. Amberes 6, Juárez, Cuauhtémoc, 06600 Ciudad de México, CDMX",
+                 description: "Cinco biciestacionamientos tipo U invertidad")
 
-Racks.create!(address: "C. Liverpool 34, Juárez, Cuauhtémoc, 06600 Juárez, CDMX",
-              description: "Cuatro biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "C. Liverpool 34, Juárez, Cuauhtémoc, 06600 Juárez, CDMX",
+                 description: "Cuatro biciestacionamientos tipo U invertidad")
 
-Racks.create!(address: "Cda. Ernesto Pugibet 17, Colonia Centro, Centro, Cuauhtémoc, 06000 Ciudad de México, CDMX",
-              description: "Tres biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "Cda. Ernesto Pugibet 17, Colonia Centro, Centro, Cuauhtémoc, 06000 Ciudad de México, CDMX",
+                 description: "Tres biciestacionamientos tipo U invertidad")
 
-Racks.create!(address: "Revillagigedo 20, Colonia Centro, Centro, Cuauhtémoc, 06050 Ciudad de México, CDMX",
-              description: "Tres biciestacionamiento tipo U invertidad")
+BikeRack.create!(address: "Revillagigedo 20, Colonia Centro, Centro, Cuauhtémoc, 06050 Ciudad de México, CDMX",
+                 description: "Tres biciestacionamiento tipo U invertidad")
 
-Racks.create!(address: "Calle Julio Verne 233, Polanco, Polanco IV Secc, Miguel Hidalgo, 11550 Ciudad de México, CDMX",
-              description: "biciestacionamiento tipo Auto")
+BikeRack.create!(address: "Calle Julio Verne 233, Polanco, Polanco IV Secc, Miguel Hidalgo, 11550 Ciudad de México, CDMX",
+                 description: "biciestacionamiento tipo Auto")
 
-Racks.create!(address: "Parroquia 517, Col del Valle Sur, Benito Juárez, 03100 Ciudad de México, CDMX",
-              description: "Cinco biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "Parroquia 517, Col del Valle Sur, Benito Juárez, 03100 Ciudad de México, CDMX",
+                 description: "Cinco biciestacionamientos tipo U invertidad")
 
-Racks.create!(address: "Torres Adalid 1359, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
-              description: "Cinco biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "Torres Adalid 1359, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
+                 description: "Cinco biciestacionamientos tipo U invertidad")
 
-Racks.create!(address: "Pedro Romero de Terreros 1102, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
-              description: "Cinco biciestacionamientos tipo U invertidad")
+BikeRack.create!(address: "Pedro Romero de Terreros 1102, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
+                 description: "Cinco biciestacionamientos tipo U invertidad")
