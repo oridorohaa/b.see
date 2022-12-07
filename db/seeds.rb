ad9594d7ab1558afdfda840b0f923efa7777bb03
Boris Moreno <larefineriadedavin@gmail.com>
2022-12-07 12:53:30 -0600

    new push for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,57 +12,71 @@
 
 # html_file = URI.open(url).read
 # html_doc = Nokogiri::HTML(html_file)
+User.destroy_all
+
+user = User.create(username: 'Joseph', email: 'joseph@email.com', password: '123456')
+
 BikeRack.destroy_all
 
 BikeRack.create!(address: "Adolfo Prieto 303, código 2, Col del Valle Centro, Benito Juárez, 03103 Ciudad de México, CDMX",
                  description: "Tres biciestacionamientos tipo cuadro",
                  latitude: 19.39550,
-                 longitude: -99.16681)
+                 longitude: -99.16681,
+                 user: user)
 
 BikeRack.create!(address: "Amsterdam 244, Hipódromo, Cuauhtémoc, 06100 Ciudad de México, CDMX",
                  description: "Un biciestacionamiento tipo U invertidad",
                  latitude: 19.41125,
-                 longitude: -99.16801)
+                 longitude: -99.16801,
+                 user: user)
 
 BikeRack.create!(address: "C. Amberes 6, Juárez, Cuauhtémoc, 06600 Ciudad de México, CDMX",
                  description: "Cinco biciestacionamientos tipo U invertidad",
                  latitude: 19.42732,
-                 longitude: -99.16599)
+                 longitude: -99.16599,
+                 user: user)
 
 BikeRack.create!(address: "C. Liverpool 34, Juárez, Cuauhtémoc, 06600 Juárez, CDMX",
                  description: "Cuatro biciestacionamientos tipo U invertidad",
                  latitude: 19.42746,
-                 longitude: -99.15753)
+                 longitude: -99.15753,
+                 user: user)
 
 BikeRack.create!(address: "Cda. Ernesto Pugibet 17, Colonia Centro, Centro, Cuauhtémoc, 06000 Ciudad de México, CDMX",
                  description: "Tres biciestacionamientos tipo U invertidad",
                  latitude: 19.43007,
-                 longitude: -99.14401)
+                 longitude: -99.14401,
+                 user: user)
 
 BikeRack.create!(address: "Revillagigedo 20, Colonia Centro, Centro, Cuauhtémoc, 06050 Ciudad de México, CDMX",
                  description: "Tres biciestacionamiento tipo U invertidad",
                  latitude: 19.43371,
-                 longitude: -99.14594)
+                 longitude: -99.14594,
+                 user: user)
 
 BikeRack.create!(address: "Calle Julio Verne 233, Polanco, Polanco IV Secc, Miguel Hidalgo, 11550 Ciudad de México, CDMX",
                  description: "biciestacionamiento tipo Auto",
                  latitude: 19.42956,
-                 longitude: -99.19691)
+                 longitude: -99.19691,
+                 user: user)
 
 BikeRack.create!(address: "Parroquia 517, Col del Valle Sur, Benito Juárez, 03100 Ciudad de México, CDMX",
                  description: "Cinco biciestacionamientos tipo U invertidad",
                  latitude: 19.37031,
-                 longitude: -99.17215)
+                 longitude: -99.17215,
+                 user: user)
 
 BikeRack.create!(address: "Torres Adalid 1359, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
                  description: "Cinco biciestacionamientos tipo U invertidad",
                  latitude: 19.38957,
-                 longitude: -99.15767)
+                 longitude: -99.15767,
+                 user: user)
 
 BikeRack.create!(address: "Pedro Romero de Terreros 1102, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
                  description: "Cinco biciestacionamientos tipo U invertidad",
                  latitude: 19.39431,
-                 longitude: -99.15960)
+                 longitude: -99.15960,
+                 user: user)
 
 Shop.destroy_all
 
@@ -72,7 +86,8 @@ Shop.create!(name: "Crankset Bicycle Shop (Trek Juárez)",
              phone_number: "+525559414753",
              description: "Delivery In-store pick-up In-store shopping Same-day delivery",
              latitude: 19.42744,
-             longitude: -99.15946)
+             longitude: -99.15946,
+             user: user)
 
 Shop.create!(name: "People for Bikes",
              address: "Zacatecas 55, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX",
@@ -81,7 +96,8 @@ Shop.create!(name: "People for Bikes",
              description: "Delivery In-store pick-up In-store shopping Same-day delivery",
              website: "http://pfb.com.mx/",
              latitude: 19.41670,
-             longitude: -99.15700)
+             longitude: -99.15700,
+             user: user)
 
 Shop.create!(name: "Chop Chop Bikes",
              address: "Amsterdam 282, Colonia Condesa, Cuauhtémoc, 06100 Ciudad de México, CDMX",
@@ -90,7 +106,8 @@ Shop.create!(name: "Chop Chop Bikes",
              description: "In-store pick-up In-store shopping Free Wifi",
              website: "http://www.chopchopbikes.com/",
              latitude: 19.41297,
-             longitude: -99.16746)
+             longitude: -99.16746,
+             user: user)
 
 Shop.create!(name: "Bikedemonium Workshop",
              address: "C. Villahermosa 29, Colonia Condesa, Cuauhtémoc, 06100 Ciudad de México, CDMX",
@@ -99,7 +116,8 @@ Shop.create!(name: "Bikedemonium Workshop",
              description: "Bicycle repair shop",
              website: "http://instagram.com/bikedemoniumworkshop",
              latitude: 19.40499,
-             longitude: -99.16987)
+             longitude: -99.16987,
+             user: user)
 
 Shop.create!(name: "Pedal and Crank Bike Shop",
              address: "Gobernador José María Tornel 44, San Miguel Chapultepec I Secc, Miguel Hidalgo, 11850 Ciudad de México, CDMX",
@@ -108,7 +126,8 @@ Shop.create!(name: "Pedal and Crank Bike Shop",
              description: "Delivery In-store pick-up In-store shopping Same-day delivery FreeWifi Debit cards NFC mobile payments",
              website: "None",
              latitude: 19.41265,
-             longitude: -99.18469)
+             longitude: -99.18469,
+             user: user)
 
 Shop.create!(name: "BICIMANIACOS BIKE STORE",
              address: "Cumbres de Maltrata 160, Narvarte Poniente, Benito Juárez, 03020 Ciudad de México, CDMX",
@@ -117,7 +136,8 @@ Shop.create!(name: "BICIMANIACOS BIKE STORE",
              description: "Delivery In-store pick-up In-store shopping Same-day delivery Gender-neutral toilets Free WiFi",
              website: "https://www.facebook.com/bicimaniacos.bikestore/",
              latitude: 19.39548,
-             longitude: -99.15480)
+             longitude: -99.15480,
+             user: user)
 
 Shop.create!(name: "LBU Bike Store Patriotismo",
              address: "Av. Patriotismo 384, San Pedro de los Pinos, Benito Juárez, 03800 Ciudad de México, CDMX",
@@ -126,7 +146,8 @@ Shop.create!(name: "LBU Bike Store Patriotismo",
              description: "In-store shopping In-store pick-up Delivery Same-day delivery Wheelchair-accessible",
              website: "http://www.lbushop.mx/",
              latitude: 19.39058,
-             longitude: -99.18256)
+             longitude: -99.18256,
+             user: user)
 
 Shop.create!(name: "Group Bcl Imports",
              address: "C. Carmen 97, Nativitas, Benito Juárez, 03500 Ciudad de México, CDMX",
@@ -135,7 +156,8 @@ Shop.create!(name: "Group Bcl Imports",
              description: "In-store shopping In-store pick-up Delivery Wheelchair accessible",
              website: "None",
              latitude: 19.38093,
-             longitude: -99.13548)
+             longitude: -99.13548,
+             user: user)
 
 Shop.create!(name: "Bike Urban Reform",
              address: "Av. Morelos 70, Juárez, Cuauhtémoc, 06600 Ciudad de México, CDMX",
@@ -144,7 +166,8 @@ Shop.create!(name: "Bike Urban Reform",
              description: "Identifies as women-owned Gender-neutral toilets LGBTQ+friendly Transgender safe space",
              website: "http://www.lbushop.mx/",
              latitude: 19.43277,
-             longitude: -99.15174)
+             longitude: -99.15174,
+             user: user)
 
 Shop.create!(name: "Hippixi Bikes",
              address: "Lamartine 607, Polanco, Bosque de Chapultepec I Secc, Miguel Hidalgo, 11580 Ciudad de México, CDMX",
@@ -153,4 +176,5 @@ Shop.create!(name: "Hippixi Bikes",
              description: "",
              website: "In-store pick-up Delivery Same-day delivery Free ",
              latitude: 19.42918,
-             longitude: -99.18985)
+             longitude: -99.18985,
+             user: user)
