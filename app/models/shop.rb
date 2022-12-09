2d98272ac18c5cbcc496688100d797c82f4c32f8
Morgan Bell <mtbell98@gmail.com>
2022-12-09 16:41:40 -0600

    added validations to models

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,6 +5,10 @@ class Shop < ApplicationRecord
   has_many :shop_reports, dependent: :destroy
   has_many_attached :photos, dependent: :destroy
 
+  validates :address, :hours, :name, presence: true
+  validates :address, uniqueness: true
+  validates :photos, presence: true, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..(5.megabytes) }
+
   def has_user_liked?(user)
     self.shop_likes.where(user_id: user.id).present?
   end
