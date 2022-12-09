fd34da6ebac3986350b9dad595861f764a97af2d
Andriana Oridoroha <oridorohaa@gmail.com>
2022-12-09 10:46:21 -0600

    reports change markers color / show page lists reports

--- app/javascript/controllers/racks_google_maps_controller.js
+++ app/javascript/controllers/racks_google_maps_controller.js
@@ -13,30 +13,43 @@ export default class extends Controller {
   initMap(){
     if (typeof google === 'undefined') return;
     let map = sharedMapsInit();
-    console.log("rack MAP", map)
+
     //custom code
     if(bike_rack_data && map){
-      console.log("bike rack data defined")
-      for(const rack in bike_rack_data){
-      let d = bike_rack_data[rack]
-      if(d.lat !== ''){
-        console.log("map")
-        console.log( parseFloat(d.lat))
-        let marker = new google.maps.Marker({
-          position: { lat: parseFloat(d.lat), lng: parseFloat(d.long) },
-          map,
-          title:"rack",
-          url: `/bike_racks/${rack}`,
-          icon: {
-              url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
-            }
 
-        })
-        marker.setMap(map);
-        google.maps.event.addListener(marker, "click", function() {
-          window.location.href = this.url;
-        });
-      }
+      for(const rack in bike_rack_data){
+        let d = bike_rack_data[rack]
+        if(d.lat !== ''){
+          if(d.status ==='true'){
+            let marker = new google.maps.Marker({
+              position: { lat: parseFloat(d.lat), lng: parseFloat(d.long) },
+              map,
+              title:"rack",
+              url: `/bike_racks/${rack}`,
+              icon: {
+                  url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+                }
+            })
+            marker.setMap(map);
+            google.maps.event.addListener(marker, "click", function() {
+              window.location.href = this.url;
+            });
+          }else{
+            let marker = new google.maps.Marker({
+              position: { lat: parseFloat(d.lat), lng: parseFloat(d.long) },
+              map,
+              title:"rack",
+              url: `/bike_racks/${rack}`,
+              icon: {
+                  url: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+                }
+            })
+            marker.setMap(map);
+            google.maps.event.addListener(marker, "click", function() {
+              window.location.href = this.url;
+            });
+          }
+        }
       }
     }
 
